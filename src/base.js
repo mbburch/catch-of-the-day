215bb2bf53a3b1bd30ab7e74f6a924da1ce3d24f
MB Burch <marybethburch@gmail.com>
2017-04-14 21:11:03 -0600

    Finished video 18

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyA8s_oMmlxPqtXxbiiN438hEOaUgIAxVpU",
+  authDomain: "catch-of-the-day-mb-burch.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-mb-burch.firebaseio.com"
+});
+
+export default base;
